788327cc0c63cd147fdcee410247384126ea1ffc
bodyshapersystem <beautyboxmia@gmail.com>
2026-09-09 15:57:49 -0400

    Add production robots directives for Google

---
+++ app/robots.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+
+const siteUrl = "https://www.gloriabeautysalonmiami.com";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/servicios",
+          "/reservar",
+          "/galeria",
+          "/equipo",
+          "/sobre-gloria",
+          "/shop",
+          "/memberships",
+          "/access",
+          "/access/book",
+          "/access/shop",
+        ],
+        disallow: [
+          "/hub",
+          "/hub/",
+          "/hub-login",
+          "/hub-reset-password",
+          "/hub-forgot-password",
+          "/api/",
+        ],
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
+  };
+}
