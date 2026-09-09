f690079ee77271ab26a8bb13952ae0c4665a8f16
bodyshapersystem <beautyboxmia@gmail.com>
2026-09-09 16:04:21 -0400

    Strengthen Google SEO foundation

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,5 @@
 import type { MetadataRoute } from "next";
-
-const siteUrl = "https://www.gloriabeautysalonmiami.com";
+import { siteUrl } from "@/lib/seo";
 
 export default function robots(): MetadataRoute.Robots {
   return {
